ac835c7dbfec0f2c699ab975960349d30169ae23
sigmabeta96 <sigmabeta96@gmail.com>
2024-05-08 14:14:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ def process():
 # Flask route to render the HTML page
 @app.route('/')
 def index():
-    return render_template('index2.html')
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
